b20f8b1255ceef5c1a660dc74c9dc05b68dc8698
Aitor Ruiz <aitorruizagra.es@gmail.com>
2026-02-20 21:49:33 -0300

    test: add unit test for node struct
    Add a unit test to verify that nodes are created correctly
    with default previous, next, and element values.

--- src/node.rs
+++ src/node.rs
@@ -19,3 +19,17 @@ impl<T> Node<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Node;
+
+    #[test]
+    fn create_node() {
+        let node = Node::new(1);
+
+        assert!(node.previous.is_none());
+        assert!(node.next.is_none());
+        assert_eq!(node.element, 1);
+    }
+}
